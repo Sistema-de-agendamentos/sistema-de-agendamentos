012afe5131fce6e4e4f3f77547f6be960d0a909d
Fernando Schindler <fernando.schindler2@gmail.com>
2023-05-10 21:17:40 -0300

    implementado o QueryClientProvider e o ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ThemeProvider } from "@mui/material";
+
+import theme from "./styles/theme";
 import Login from "./screens/Login";
 
+import "./styles/styles.css";
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <Login />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Login />
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
