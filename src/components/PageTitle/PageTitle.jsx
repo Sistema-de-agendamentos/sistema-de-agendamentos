ad05ea793f6d87ba5d257778b3b715cbe7e722c0
Fernando Schindler <fernando.schindler2@gmail.com>
2023-05-14 15:13:47 -0300

    realizado uma pequena correção no componente: PageTitle

--- src/components/PageTitle/PageTitle.jsx
+++ src/components/PageTitle/PageTitle.jsx
@@ -1,13 +1,13 @@
 import { styled } from "@mui/material";
 import Typography from "@mui/material/Typography";
 
-const StyledTypography = styled(Typography)(({ removeLine }) => ({
+const StyledTypography = styled(Typography)((props) => ({
   marginBottom: "0.5rem",
   fontSize: "1.5rem",
   fontWeight: "bold",
   alignSelf: "flex-start",
   color: "#333",
-  ":after": !removeLine && {
+  ":after": !props["data-remove-line"] && {
     content: "''",
     display: "block",
     width: "70%",
@@ -19,7 +19,7 @@ const StyledTypography = styled(Typography)(({ removeLine }) => ({
 
 function PageTitle({ title = null, removeLine = false }) {
   return (
-    <StyledTypography variant="h1" removeLine={removeLine}>
+    <StyledTypography variant="h1" data-remove-line={removeLine}>
       {title}
     </StyledTypography>
   );
