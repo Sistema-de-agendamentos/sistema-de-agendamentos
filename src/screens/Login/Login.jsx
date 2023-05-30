c7899e8c622bb7072bf9aef1d073183b9a432280
Fernando Schindler <fernando.schindler2@gmail.com>
2023-05-30 19:15:00 -0300

    finalizado com a criação da tela de login

--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -1,9 +1,10 @@
-import { useCallback } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router";
 import { FormProvider, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
+import useMutation from "../../hooks/useMutation";
 import Button from "../../components/Button";
 import TextField from "../../components/TextField";
 import PublicScreen from "../../components/PublicScreen";
@@ -20,26 +21,57 @@ const schema = yup.object().shape({
 
 function Login() {
   const methods = useForm({ defaultValues, resolver: yupResolver(schema) });
-  const { handleSubmit } = methods;
+  const { clearErrors, handleSubmit } = methods;
 
   const navigate = useNavigate();
 
-  const submit = useCallback(() => {
-    // eslint-disable-next-line no-console
-    console.log("submit");
-  }, []);
+  const {
+    data = {},
+    mutate,
+    isLoading,
+    isSuccess,
+  } = useMutation({
+    endpoint: "/auth",
+    successText: "Login efetuado com sucesso",
+  });
+
+  const { data: user = {} } = data;
+  const { accessToken } = user;
+
+  useEffect(() => {
+    if (isSuccess) {
+      localStorage.setItem("user", JSON.stringify(user));
+      if (accessToken) navigate("/agendamentos");
+    }
+  }, [accessToken, user, isSuccess, navigate]);
 
   return (
     <PublicScreen title="Login">
       <FormProvider {...methods}>
-        <form onSubmit={handleSubmit(submit)}>
-          <TextField name="login" label="Usuário" />
-          <TextField name="password" label="Senha" />
-          <Button type="submit" onClick={() => {}} fullWidth>
+        <form onSubmit={handleSubmit(mutate)}>
+          <TextField name="login" label="Usuário" disabled={isLoading} />
+
+          <TextField
+            name="password"
+            label="Senha"
+            disabled={isLoading}
+            type="password"
+          />
+
+          <Button
+            type="submit"
+            onClick={clearErrors}
+            isLoading={isLoading}
+            fullWidth
+          >
             Entrar
           </Button>
 
-          <Button onClick={() => navigate("/esqueceu-senha")} variant="inherit">
+          <Button
+            onClick={() => navigate("/esqueceu-senha")}
+            disabled={isLoading}
+            variant="inherit"
+          >
             Esqueceu a senha?
           </Button>
         </form>
