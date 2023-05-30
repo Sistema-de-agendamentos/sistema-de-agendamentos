1bead4287e69748226fe6370b71f392a67171830
Fernando Schindler <fernando.schindler2@gmail.com>
2023-05-29 21:43:56 -0300

    criado o componente: App

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import AuthProvider from "./AuthProvider";
+import Button from "./Button";
+import Layout from "./Layout";
+import PageTitle from "./PageTitle";
+import PublicScreen from "./PublicScreen";
+import Select from "./Select";
+import TextField from "./TextField";
+
+export {
+  AuthProvider,
+  Button,
+  Layout,
+  PageTitle,
+  PublicScreen,
+  Select,
+  TextField,
+};
