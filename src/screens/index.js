f9baee6101659eeb608c1049f8ddcee66a19bee0
Fernando Schindler <fernando.schindler2@gmail.com>
2023-06-07 19:57:01 -0300

    criado o arquivo index.js usado para exportar as telas

---
+++ src/screens/index.js
@@ -0,0 +1,7 @@
+import Agendamentos from "./Agendamentos";
+import ErrorPage from "./ErrorPage";
+import LoadingPage from "./LoadingPage";
+import Login from "./Login";
+import RecuperarSenha from "./RecuperarSenha";
+
+export { Agendamentos, ErrorPage, LoadingPage, Login, RecuperarSenha };
