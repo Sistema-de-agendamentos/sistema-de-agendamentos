d5673f46db147391b64bc60c7bdc3b80d1544bf6
Fernando Schindler <fernando.schindler2@gmail.com>
2023-06-07 19:58:54 -0300

    adicionado novas rotas no arquivo: private.routes.js

--- src/routes/private.routes.js
+++ src/routes/private.routes.js
@@ -1,5 +1,11 @@
+import NotFound from "../screens/NotFound";
 import Agendamentos from "../screens/Agendamentos";
+import Clientes from "../screens/Clientes";
 
-const privateRoutes = [{ element: <Agendamentos />, path: "/agendamentos" }];
+const privateRoutes = [
+  { element: <NotFound />, path: "/404" },
+  { element: <Agendamentos />, path: "/agendamentos" },
+  { element: <Clientes />, path: "/clientes" },
+];
 
 export default privateRoutes;
