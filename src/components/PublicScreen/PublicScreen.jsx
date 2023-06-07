da100cf232c541a1923f093cc70801c15e3e0da5
Fernando Schindler <fernando.schindler2@gmail.com>
2023-06-07 19:54:04 -0300

    realizado pequenas correções na estilização

--- src/components/PublicScreen/PublicScreen.jsx
+++ src/components/PublicScreen/PublicScreen.jsx
@@ -11,7 +11,7 @@ function PublicScreen({ children, title }) {
   return (
     <StyledContainer>
       <StyledContentLeft>
-        <img src={Logo} alt="Logo" style={{ filter: "grayscale(1)" }} />
+        <img src={Logo} alt="Logo" className="logo" />
       </StyledContentLeft>
 
       <StyledContentRight>
