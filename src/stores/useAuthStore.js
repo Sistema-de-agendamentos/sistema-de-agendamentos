1d1f7ec91baccbedd3553b8ab61f741069cbea07
Fernando Schindler <fernando.schindler2@gmail.com>
2023-07-12 13:57:27 -0300

    implementado para salvar o usuário logado no estado global

--- src/stores/useAuthStore.js
+++ src/stores/useAuthStore.js
@@ -2,7 +2,9 @@ import create from "zustand";
 
 const useAuthStore = create((set) => ({
   isAuthenticated: false,
+  user: {},
   setAuthentication: (value) => set({ isAuthenticated: value }),
+  setUser: (value) => set({ user: value }),
 }));
 
 export default useAuthStore;
