57dabf412c613987ab67ab296316a7ce0b7406e6
Fernando Schindler <fernando.schindler2@gmail.com>
2023-08-23 20:25:20 -0300

    realizado uma pequena correção no método: date, do util: addMask

--- src/utils/addMask.js
+++ src/utils/addMask.js
@@ -5,7 +5,7 @@ function phone(value) {
 
 function date(value) {
   const cleanValue = value.replace(/\D/g, "");
-  return cleanValue.replace(/(\d{2})(\d{1})(\d{4})(\d{4})/g, "($1) $2 $3-$4");
+  return cleanValue.replace(/(\d{4})(\d{2})(\d{2})/g, "$3/$2/$1");
 }
 
 export { date, phone };
