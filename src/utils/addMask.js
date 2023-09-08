e0e134a0d86e3564f277b5f39d3be8823fd0076d
Fernando Schindler <fernando.schindler2@gmail.com>
2023-09-08 00:50:57 -0300

    adicionado novos tratamentos no util: addMask

--- src/utils/addMask.js
+++ src/utils/addMask.js
@@ -1,9 +1,11 @@
 function phone(value) {
+  if (typeof value !== "string") return "";
   const cleanValue = value.replace(/\D/g, "");
   return cleanValue.replace(/(\d{2})(\d{1})(\d{4})(\d{4})/g, "($1) $2 $3-$4");
 }
 
 function date(value) {
+  if (typeof value !== "string") return "";
   const cleanValue = value.replace(/\D/g, "");
   return cleanValue.replace(/(\d{4})(\d{2})(\d{2})/g, "$3/$2/$1");
 }
