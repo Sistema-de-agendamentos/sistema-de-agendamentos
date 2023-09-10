214c8c17b7aefcf7502f23d15cedb37a7763def8
Fernando Schindler <fernando.schindler2@gmail.com>
2023-09-10 14:40:15 -0300

    corrigido o método: date, do util: addMask

--- src/utils/addMask.js
+++ src/utils/addMask.js
@@ -6,7 +6,7 @@ function phone(value) {
 
 function date(value) {
   if (typeof value !== "string") return "";
-  const cleanValue = value.replace(/\D/g, "");
+  const cleanValue = value.replace(/\D/g, "").slice(0, 8);
   return cleanValue.replace(/(\d{4})(\d{2})(\d{2})/g, "$3/$2/$1");
 }
 
