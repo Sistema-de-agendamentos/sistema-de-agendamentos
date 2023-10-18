8e715f925ac99df763c34c779dd8877b5ee9be9a
Fernando Schindler <fernando.schindler2@gmail.com>
2023-10-18 20:58:44 -0300

    criado máscaras no componente: TextField

--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -2,13 +2,12 @@ import { useCallback, useMemo } from "react";
 import { useFormContext, Controller } from "react-hook-form";
 import { TextField as TextFieldMaterialUI } from "@mui/material";
 
-function TextField({ name, label, type = "text", disabled, ...props }) {
+function TextField({ name, label, type = "text", disabled, mask, ...props }) {
   const {
     control,
     clearErrors,
     formState: { errors },
   } = useFormContext();
-
   const errorMessage = errors[name]?.message;
 
   const clearError = useCallback(() => clearErrors(name), [clearErrors, name]);
@@ -16,6 +15,27 @@ function TextField({ name, label, type = "text", disabled, ...props }) {
   const style = useMemo(() => {
     return { marginBottom: errorMessage ? "-0.5rem" : "0.9375rem" };
   }, [errorMessage]);
+
+  const maxLength = { cpf: 14, cell: 16, phone: 14 };
+  const maxLengthWithoutMask = { cpf: 11, cell: 11, phone: 10 };
+
+  const format = (value) => {
+    const cleanValue = value.replace(/\D/g, "");
+
+    const formattedValue = {
+      cpf: cleanValue.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4"),
+      cell: cleanValue.replace(
+        /(\d{2})(\d{1})(\d{4})(\d{4})/,
+        "($1) $2 $3-$4"
+      ),
+      phone: cleanValue.replace(
+        /(\d{2})(\d{4})(\d{4})/,
+        "($1) $2-$3"
+      ),
+    };
+
+    return value.length < maxLengthWithoutMask[mask] ? cleanValue : formattedValue[mask] || value;
+  };
 
   return (
     <Controller
@@ -29,12 +49,14 @@ function TextField({ name, label, type = "text", disabled, ...props }) {
           error={!!errorMessage}
           helperText={errorMessage}
           onFocus={clearError}
+          inputProps={{ maxLength: maxLength[mask] || 255 }}
           margin="normal"
           variant="outlined"
           fullWidth
           style={style}
           {...field}
           {...props}
+          value={format(field.value)}
         />
       )}
     />
